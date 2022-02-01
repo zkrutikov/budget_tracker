23bd139d720a62fa156c8fbcffe5bc494d54e4e0
Zak Krutikov <zak.krutikov@gmail.com>
2022-01-31 20:03:41 -0500

    worked on the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTraker';     
 const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION
+const DATA_CACHE_NAME = 'data-cache-' + 'v1'
 const FILES_TO_CACHE = [
   "/",
   "./index.html",
@@ -19,23 +20,45 @@ const FILES_TO_CACHE = [
 ];
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        }
+      });
     })
-  )
-})
+  );
+});
 
 // Cache resources
 self.addEventListener('install', function (e) {
